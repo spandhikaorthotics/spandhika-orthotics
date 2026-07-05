173cd0f8b1c8e0ee3f0673c9a9748973b1fbfdbb
spandhikaorthotics <spandhikaorthotics@gmail.com>
2026-07-06 00:53:00 +0530

    [fix]: typo

--- src/components/ui/TargetAudience.tsx
+++ src/components/ui/TargetAudience.tsx
@@ -37,7 +37,7 @@ export default function TargetAudience() {
     {
       title: "Everyday Seniors",
       subtitle: "Aging Feet & Daily Walks",
-      image: "/elderly.png",
+      image: "/eldery.png",
       bullets: [
         "Restore alignment",
         "Shock-absorbing support",
