35ce4cf20c90f4be6cafe9a02825cda03ae7ab65
spandhikaorthotics <spandhikaorthotics@gmail.com>
2026-07-10 22:48:34 +0530

    fix: update sitemap URL for waitlist to blog

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,7 +8,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://spandhikaorthotics.in/waitlist",
+      url: "https://spandhikaorthotics.in/blog",
       lastModified: new Date(),
       priority: 0.8,
     },
